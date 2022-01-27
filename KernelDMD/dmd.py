3223d88c5345980fead0d57c432bd8da1638f044
Pietronvll <pietronvll@gmail.com>
2022-01-27 13:54:46 +0100

    modified DMD

--- KernelDMD/dmd.py
+++ KernelDMD/dmd.py
@@ -13,41 +13,41 @@ def _get_scaled_SVD_right_eigvectors(X, num_modes, backend):
     else:
         raise ValueError("Supported backends are 'torch' or 'keops'")
 
-def _DMD(trajectory, kernel, num_modes, backend):
-    """DMD utility function
+def _DMD(trajectory, evolved_trajectory, kernel, num_modes, backend):
+    """DMD using truncated SVD decomposition
 
     Args:
         trajectory (array): [observations, features]
         kernel (kernel object)
         num_modes (int): number of modes to compute
     """
-    Vhat_r = _get_scaled_SVD_right_eigvectors(kernel(trajectory[:-1], backend=backend), num_modes, backend)
-    Ahat = Vhat_r.T @(kernel(trajectory[:-1],trajectory[1:], backend=backend)@Vhat_r)
+    Vhat_r = _get_scaled_SVD_right_eigvectors(kernel(trajectory, backend=backend), num_modes, backend)
+    Ahat = Vhat_r.T @(kernel(trajectory,evolved_trajectory, backend=backend)@Vhat_r)
     if backend =='torch':
         evals, evecs = torch.linalg.eig(Ahat)
-        Vhat_r = Vhat_r.cfloat() #convert to Complex type
+        Vhat_r = Vhat_r.type(evecs.type()) #convert to Complex type
     elif backend =='keops':
         evals, evecs = numpy.linalg.eig(Ahat)
     else:
         raise ValueError("Supported backends are 'torch' or 'keops'")    
     return evals, Vhat_r@evecs
 
-def DMD_large_scale(trajectory, kernel, num_modes):
-    """DMD using Lanczos iterations. Useful for large problems
+def DMDs(trajectory, evolved_trajectory, kernel, num_modes):
+    """Sparse DMD using Lanczos iterations. Useful for large problems
 
     Args:
         trajectory (array): [observations, features]
         kernel (kernel object)
         num_modes (int): number of modes to compute
     """
-    return _DMD(trajectory, kernel, num_modes, 'keops')
+    return _DMD(trajectory, evolved_trajectory, kernel, num_modes, 'keops')
 
-def DMD(trajectory, kernel, num_modes):
-    """DMD using truncated SVD
+def DMD(trajectory, evolved_trajectory, kernel, num_modes):
+    """Sparse DMD using Lanczos iterations. Useful for large problems
 
     Args:
         trajectory (array): [observations, features]
         kernel (kernel object)
         num_modes (int): number of modes to compute
     """
-    return _DMD(trajectory, kernel, num_modes, 'torch')
+    return _DMD(trajectory, evolved_trajectory, kernel, num_modes, 'torch')
