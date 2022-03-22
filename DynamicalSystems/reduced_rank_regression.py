de7705ed336936dbddd8fe588c80c6311188dd0c
Pietro Novelli <pietro.novelli@iit.it>
2022-03-22 11:41:01 +0100

    refactored implementatio

--- DynamicalSystems/reduced_rank_regression.py
+++ DynamicalSystems/reduced_rank_regression.py
@@ -1,83 +1,93 @@
-from turtle import back
+from multiprocessing.sharedctypes import Value
 from attr import evolve
-from scipy.sparse.linalg import eigs, aslinearoperator, LinearOperator
+from scipy.sparse.linalg import eigs, eigsh, aslinearoperator, LinearOperator, cg
 from scipy.linalg import eig
 from scipy.sparse import diags, identity
-from pykeops.numpy import Vi
+from pykeops.numpy import Vj
 import numpy as np
 from warnings import warn
+import tqdm
     
-def reduced_rank_regression(data, evolved_data, kernel, rank, regularizer=1e-8, center_kernel = False, _cumbersome_diagonalization = True):
-
+def reduced_rank_regression(data, evolved_data, kernel, rank, regularizer=None, center_kernel = False):
     # Defining kernels
-    print("Evaluating Kernel")
-    data_kernel = kernel(data, data)
-    print("Done") 
+    data_kernel = aslinearoperator(kernel(data, data))
     evolved_data_kernel = aslinearoperator(kernel(evolved_data, evolved_data))
-    print("Done")
     #Working with the transpose of the matrix in the notes for easier implementation
     cross_kernel = aslinearoperator(kernel(data, evolved_data))
-    print("Done")
-    
-    dim = data_kernel.shape[0]
-    scale = np.array(dim**-1).astype(data_kernel.dtype)    
-
+  
     #Correcting Kernel if needed
     if center_kernel:
         evolved_data_kernel += _center_kernel(kernel, evolved_data, evolved_data, data, averaged_indices=(True, True))
         cross_kernel += _center_kernel(kernel, data, evolved_data, data, averaged_indices=(False, True))
-        print("Done")
-    print("Low rank proj")
-    proj = _get_low_rank_projector(aslinearoperator(data_kernel*scale.item()), evolved_data_kernel, rank, regularizer)
-    print("Done")
+    
+    print("Low rank projection")
+    proj = np.asfortranarray(_low_rank_projector(data_kernel, evolved_data_kernel, rank, regularizer))
+    dim = proj.shape[0]
     #Neither U nor V are normalised by 1/dim because from _get_low_rank_projector we spare a factor dim
     U = cross_kernel.matmat(proj)
     U = U.T #[rank, dim]
-    V = evolved_data_kernel.matmat(proj) # [dim, rank]
-
-    if _cumbersome_diagonalization:    
-        # V@(U.T)w =  λ K_reg w
-        B = aslinearoperator(data_kernel) + aslinearoperator(diags(np.ones(dim, dtype=U.dtype)*(regularizer*dim)))
-        return eigs(aslinearoperator(V)@aslinearoperator(U), rank, B)
+    if regularizer is None:
+        T = proj
+    else:        
+        T = np.asfortranarray(evolved_data_kernel.matmat(proj)) # [dim, rank]
+    print("Final diagonalization")
+    
+    # U@K^-1@T w = λw
+    # C := K_reg^-1@V
+    if regularizer is None:
+        alpha = 1e-10 #Very small reg parameter to solve least squares.
     else:
-        # U@K_reg^-1@V w = λw
-        # C := K_reg^-1@V
-        C = data_kernel.solve(Vi(V), alpha=regularizer*dim, eps=1e-12)
-        vals, vecs =  eig(U@C)
-        return vals, C@vecs
+        alpha = regularizer*dim
+    
+    K = aslinearoperator(diags(alpha * np.ones(dim, dtype=data_kernel.dtype), dtype=data_kernel.dtype)) + data_kernel
+    C = np.empty_like(proj)
+    for i in tqdm.tqdm(range(rank), desc = "Solving CG iteration"):
+        x, info = cg(K, T[:,i], tol=1e-10)
+        if info > 0:
+            warn("Convergence not achieved")
+            C[:,i] = 0
+        elif info < 0:
+            raise ValueError("Illegal input or breakdown")
+        C[:, i] = x
+    #C = kernel(data).solve(Vj(T), alpha=alpha, sum_scheme='kahan_scheme')
+    vals, vecs =  eig(U@C)
+    return vals, C@vecs
 
-def _get_low_rank_projector(data_kernel, evolved_data_kernel, rank, regularizer):
+def _low_rank_projector(data_kernel, evolved_data_kernel, rank, regularizer):
     #For the moment data_kernel = LinearOperator, evolved_data_kernel = LinearOperator
     dim = data_kernel.shape[0]
+    inverse_dim = dim**-1
 
-    KK_P = data_kernel@evolved_data_kernel
-    tikhonov_reg = aslinearoperator(diags(np.ones(dim, dtype=KK_P.dtype)*(regularizer*dim)))
-    K_reg = data_kernel + tikhonov_reg
-    _, V = eigs(KK_P, rank, K_reg)
-    
+    if regularizer is None:
+        #Momentary solution
+        _, V = eigsh(evolved_data_kernel, rank)
+    else:
+        K = inverse_dim*((data_kernel.T)@evolved_data_kernel)     
+        tikhonov = aslinearoperator(diags(np.ones(dim, dtype=K.dtype)*(regularizer*dim)))
+        _, V = eigs(K, rank, data_kernel + tikhonov)
+        
+        #Check that the eigenvectors are real (or have a global phase at most)
+        if not _check_real(V):
+            V_global_phase_norm = np.angle(V).std()
+            if V_global_phase_norm  > 1e-8:
+                raise ValueError("Computed projector is not real. The kernel function is either severely ill conditioned or non-symmetric")
+            else:
+                #It has a global complex phase, take absolute.
+                V = np.abs(V)
+        else:
+            V = np.real(V)
+            V = modified_QR(V, evolved_data_kernel)
+        #V = modified_QR(V, evolved_data_kernel)
+
     #V now are orthonormal w.r.t. K_P and not K_P/dim. They should be normalised by multiplying by sqrt(dim). Let's skip this and lose a dim factor up
     #V *= np.sqrt(dim, dtype = V.dtype) 
-
-    #Check that the eigenvectors are real (or have a global phase at most)
-    if not _check_real(V):
-        V_global_phase_norm = np.angle(V).std()
-        if V_global_phase_norm  > 1e-8:
-            print(f"Global Phase is ~ {np.around(np.angle(V).mean(), decimals = 3)} ± {np.around(V_global_phase_norm, decimals = 3)} ")
-            warn(f"Computed projector is not real, chopping imaginary part.")
-            V = np.real(V)
-        else:
-            #It has a complex phase, take absolute.
-            V = np.abs(V)
-    else:
-        V = np.real(V)
-    return modified_QR(V, evolved_data_kernel)
+    return V
 
 def _check_real(V, eps = 1e-8):
     if np.max(np.abs(np.imag(V))) > eps:
         return False
     else:
-        return True
-       
+        return True 
 
 def _center_kernel(kernel, X, Y, D, averaged_indices):
     K = kernel(X, Y) 
@@ -104,7 +114,8 @@ def _center_kernel(kernel, X, Y, D, averaged_indices):
             W = w.sum()
             #Default choice average both indices
             return (-K_X*W - np.dot(w, K_Y) + W*K_D*scale)*scale
-    return LinearOperator(K.shape, matvec =  _matvec)   
+
+    return LinearOperator(K.shape, matvec =  _matvec, dtype= K.dtype)   
 
 def modified_QR(A, M=None):
     dim = A.shape[0]
@@ -113,7 +124,7 @@ def modified_QR(A, M=None):
         M = identity(dim, dtype= A.dtype)
     Q = np.zeros(A.shape, dtype=A.dtype)
     for j in range(0, vecs):
-        q = A[:,j]
+        q = np.asfortranarray(A[:,j])
         for i in range(0, j):
             rij = np.vdot(Q[:,i], M@q)
             q = q - rij*Q[:,i]
