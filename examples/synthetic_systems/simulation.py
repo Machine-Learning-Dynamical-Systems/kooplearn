b0f7a0fd8b23307c1415c4a5151a1546b4a4a77e
Pietro Novelli <pietro.novelli@iit.it>
2022-06-21 10:20:12 +0200

    [examples] noisy logistic map saving updates

--- examples/synthetic_systems/simulation.py
+++ examples/synthetic_systems/simulation.py
@@ -1,14 +1,11 @@
 import numpy as np
-import matplotlib.pyplot as plt
-from scipy.integrate import quad
-from utils import LogisticMap, CosineKernel, LogisticMapSimulation
+from utils import LogisticMapSimulation
 import sys
 sys.path.append("../../")
-from DynamicalSystems.koopman_regression import KernelRidgeRegression, ReducedRankRegression, PrincipalComponentRegression, RandomizedReducedRankRegression
+from DynamicalSystems.koopman_regression import KernelRidgeRegression, ReducedRankRegression, PrincipalComponentRegression 
 from DynamicalSystems.kernels import RBF
 
 N = 20
-kernel = CosineKernel(N)
 kernel = RBF(length_scale = 0.5)
 regularization_size = 30
 tikhonov_regs = np.geomspace(1e-7, 1e-2, num=regularization_size)
@@ -17,7 +14,14 @@ parameters = {
     'num_train_samples' :   10000,
     'ranks' :               3,
     'tikhonov_regs':        tikhonov_regs,
-    'estimators' :          [KernelRidgeRegression],
+    'estimators' :          [KernelRidgeRegression, ReducedRankRegression],
+}
+
+parameters = {
+    'num_train_samples' :   100,
+    'ranks' :               3,
+    'tikhonov_regs':        tikhonov_regs,
+    'estimators' :          [KernelRidgeRegression, ReducedRankRegression],
 }
 
 statistics = {
@@ -28,8 +32,5 @@ statistics = {
 
 if __name__ == "__main__":
     simulation = LogisticMapSimulation(kernel, parameters, statistics, N = N)
-    train_errors, test_errors, eigvals = simulation.run_eigs(backend='keops', num = 10)
-    np.save('_tmp_eigenvalues.npy', eigvals)
-    np.save('_tmp_train_errors.npy', train_errors)
-    np.save('_tmp_test_errors.npy', test_errors)
+    train_errors, test_errors, eigvals = simulation.run_eigs(backend='cpu', num = 10, save=True, save_path = "../../../data/noisy_logistic_map/")
 
