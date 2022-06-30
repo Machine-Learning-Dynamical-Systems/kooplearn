5614fc4dbdc5a8cf91599cc8a5647767c8153394
Pietro Novelli <pietro.novelli@iit.it>
2022-06-30 12:05:31 +0200

    Updated logistic map simulations

--- examples/synthetic_systems/simulation.py
+++ examples/synthetic_systems/simulation.py
@@ -7,7 +7,7 @@ from DynamicalSystems.kernels import RBF
 
 N = 20
 kernel = RBF(length_scale = 0.5)
-regularization_size = 10
+regularization_size = 1
 tikhonov_regs = np.geomspace(1e-6, 1e-3, num=regularization_size)
 
 parameters = {
@@ -17,6 +17,13 @@ parameters = {
     'estimators' :          [KernelRidgeRegression, ReducedRankRegression],
 }
 
+# parameters = {
+#     'num_train_samples' :   10000,
+#     'ranks' :               3,
+#     'tikhonov_regs':        tikhonov_regs,
+#     'estimators' :          [PrincipalComponentRegression],
+# }
+
 statistics = {
     'num_test_samples' : 500,
     'test_repetitions' : 1,
@@ -25,5 +32,4 @@ statistics = {
 
 if __name__ == "__main__":
     simulation = LogisticMapSimulation(kernel, parameters, statistics, N = N)
-    train_errors, test_errors, eigvals = simulation.run_eigs(backend='cpu', num = 10, save=True, save_path = "../../../data/noisy_logistic_map/")
-
+    train_errors, test_errors, eigvals = simulation.run_eigs(backend='keops', num = 10, save=True, save_path = "../../../data/noisy_logistic_map/")
