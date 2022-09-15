bb9c2453742755a15a4379dad264d2deb23b7f26
Pietro Novelli <pietro.novelli@iit.it>
2022-09-15 15:00:25 +0200

    Modified Kernel module

--- kooplearn/kernels.py
+++ kooplearn/kernels.py
@@ -19,7 +19,35 @@ class Kernel(metaclass=ABCMeta):
     @abstractmethod
     def __call__(self, X, Y=None, backend='numpy'):
         """Evaluate the kernel."""
-    
+
+class FiniteDimensionalKernel(Kernel):
+    def __call__(self, X, Y = None, backend = 'numpy'):
+        """Return the Kernel matrix k(x,y) := <Phi(x),Phi(y)>. Different samples are batched on the first dimension of X and Y."""
+        phi_X = self.__feature_map__(X)
+        if Y is None:
+            phi_Y = None
+            if backend == 'numpy':
+                return phi_X@(phi_X.T)
+        else:
+            phi_Y = self.__feature_map__(Y)
+            if backend == 'numpy':
+                return phi_X@(phi_Y.T)
+        return lazy_cprod(phi_X, phi_Y) #Backend == 'keops'
+
+    def cov(self, X, Y = None):
+        phi_X = self.__feature_map__(X)
+        if Y is None:
+            return phi_X.T@phi_X
+        else:
+            if X.shape[0] != Y.shape[0]:
+                raise ValueError("Shape mismatch: cross-covariances can be computed only if X.shape[0] == Y.shape[0] ")
+            phi_Y = self.__feature_map__(Y)
+            return phi_X.T@phi_Y
+    @abstractmethod
+    def __feature_map__(self, X, backend='numpy'):
+        """Evaluate the feature map. The output should be a numpy array in _any_ case.""" 
+        return X #Linear Kernel 
+
 class RBF(Kernel):
     def __init__(self, length_scale=1.0):
         self.length_scale = length_scale 
@@ -31,7 +59,6 @@ class RBF(Kernel):
             return (-(lazy_cdist(X,Y)** 2) / (2*(Pm(self.length_scale)**2))).exp()
         else:
             return self._scikit_kernel(X, Y)
-            
 class Matern(Kernel):
     def __init__(self, nu=1.5, length_scale=1.0):
         self.nu = nu
@@ -80,12 +107,10 @@ class Poly(Kernel):
             else:
                 raise NotImplementedError("Poly kernel with degree != [1,2] not implemented (because of a bug on pow function).")
         else:
-            return sk_poly(X, Y, degree = self.degree, gamma = self.gamma, coef0 = self.coef0)
-            
+            return sk_poly(X, Y, degree = self.degree, gamma = self.gamma, coef0 = self.coef0)           
 class Linear(Poly):
     def __init__(self, gamma=None, coef0=1):
         super().__init__(degree=1, gamma=gamma, coef0=coef0)
-
 class Quadratic(Poly):
     def __init__(self, gamma=None, coef0=1):
         super().__init__(degree=2, gamma=gamma, coef0=coef0)
