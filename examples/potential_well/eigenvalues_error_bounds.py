76fbf47293773ca37b8f1c1b00df98fb3b49d65f
Pietro Novelli <pietro.novelli@iit.it>
2022-12-18 17:17:44 +0100

    Added data-dependent bounds

--- examples/potential_well/eigenvalues_error_bounds.py
+++ examples/potential_well/eigenvalues_error_bounds.py
@@ -7,20 +7,23 @@ import numpy as np
 #MISC
 from kooplearn.kernels import Kernel
 def error_decay_variable(n: int, delta: float, kernel_sup_norm: float = 1.0) -> float:
-    return (n**-1)*kernel_sup_norm*np.log(np.pow(n, 2)*8*(delta**-1))
+    return (n**-1)*kernel_sup_norm*np.log(np.power(n, 2)*8*(delta**-1))
 
 def hat_eps_n(X: np.ndarray, kernel: Kernel, delta: float, kernel_sup_norm: float = 1.0) -> float:
-    J = error_decay_variable(X.shape[0], delta, kernel_sup_norm=kernel_sup_norm)
-    cov_norm = np.linalg.norm(kernel(X), ord=2) #Operator norm of the Gram Matrix
+    n = X.shape[0]
+    J = error_decay_variable(n, delta, kernel_sup_norm=kernel_sup_norm)
+    cov_norm = np.linalg.norm((n**-1)*kernel(X), ord=2) #Operator norm of the Gram Matrix
     return 91.0*J + 6.0*np.sqrt(cov_norm*J)
 
 def eps_n_1(X: np.ndarray, delta:float, tikhonov_reg:float, kernel_sup_norm: float = 1.0) -> float:
-    J = error_decay_variable(X.shape[0], delta, kernel_sup_norm=kernel_sup_norm)
+    n = X.shape[0]
+    J = error_decay_variable(n, delta, kernel_sup_norm=kernel_sup_norm)
     return 12.0*(tikhonov_reg**-1)*J + 6.0*np.sqrt((tikhonov_reg**-1)*J)
 
 def hat_eps_n_2(X: np.ndarray, kernel: Kernel, delta:float, tikhonov_reg:float, kernel_sup_norm: float = 1.0) -> float:
-    J = error_decay_variable(X.shape[0], delta, kernel_sup_norm=kernel_sup_norm)
-    cov_norm = np.linalg.norm(kernel(X), ord=2) #Operator norm of the Gram Matrix
+    n = X.shape[0]
+    J = error_decay_variable(n, delta, kernel_sup_norm=kernel_sup_norm)
+    cov_norm = np.linalg.norm((n**-1)*kernel(X), ord=2) #Operator norm of the Gram Matrix
     return 12.0*(tikhonov_reg**-0.5)*J + 17.0*(tikhonov_reg**-0.25)*J*(kernel_sup_norm**0.5) + 6.0*np.sqrt((tikhonov_reg**-0.25)*J*(cov_norm**0.5))
 
 def eigenvalue_errors(
@@ -36,8 +39,13 @@ def eigenvalue_errors(
     E_n_1 = 1.0 - eps_n_1(X, _d, tikhonov_reg, kernel_sup_norm=kernel_sup_norm)
     E_n_2 = 2.0*hat_eps_n_2(X, kernel, _d, tikhonov_reg,kernel_sup_norm=kernel_sup_norm)
 
-    numerator = np.sqrt(tikhonov_reg)*E_n_1 + E_n_2 + sval_B_rp1*np.sqrt(E_n_1)
-    denumerator = E_n_1*np.sqrt(eta - tikhonov_reg - hat_eps_n(X, kernel, _d, kernel_sup_norm=kernel_sup_norm))
+    if E_n_1 < 0:
+        _err = np.empty_like(eta)
+        _err[:] = np.nan
+        return _err
+    else:
+        numerator = np.sqrt(tikhonov_reg)*E_n_1 + E_n_2 + sval_B_rp1*np.sqrt(E_n_1)
+        denumerator = E_n_1*np.sqrt(eta - tikhonov_reg - hat_eps_n(X, kernel, _d, kernel_sup_norm=kernel_sup_norm))
 
-    return numerator*(denumerator**-1)
+        return numerator*(denumerator**-1)
 
