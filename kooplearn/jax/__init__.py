bc24e2abb206b1289f132262bc85d23725615eb9
Pietro Novelli <pietro.novelli@iit.it>
2023-03-13 11:40:13 +0100

    Added JAX estimators for the primal

---
+++ kooplearn/jax/__init__.py
@@ -0,0 +1,3 @@
+from kooplearn.jax.estimators import reduced_rank_regression, randomized_reduced_rank_regression, tikhonov_regression, iterative_regression
+
+__all__ = ["reduced_rank_regression", "randomized_reduced_rank_regression", "tikhonov_regression", "iterative_regression"]
