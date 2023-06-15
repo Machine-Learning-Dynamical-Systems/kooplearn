e84943aeb0c72c76fc5b9b9b2a20f606b14b5b0a
Bruno <bruno.belucci-teixeira@dauphine.eu>
2023-06-15 18:23:29 +0200

    Refactory of Runner as a FeatureMap

--- kooplearn/main.py
+++ kooplearn/main.py
@@ -6,15 +6,16 @@ from estimators import BaseEstimator
 
 from sklearn.utils.validation import check_is_fitted
 
+
 class GeneralModel():
     def __init__(self):
         # base behaviour uses default objects, which don't alter the input's representation
-        self.phi_ = FeatureMap()
+        self.feature_map = FeatureMap()
         self.decoder_ = Decoder()
         self.is_koop_fitted = False
 
     def set_feature_map(self, phi:FeatureMap):
-        self.phi_ = phi
+        self.feature_map = phi
 
     def set_koopman(self, koop:BaseEstimator):
         self.koop_ = koop
@@ -23,16 +24,17 @@ class GeneralModel():
         self.decoder_ = decoder
 
     def fit_feature_map(self, X,Y):
-        self.phi_.fit(X,Y)
+        self.feature_map.initialize(self.koop_, self.decoder_, self.dataset)
+        self.feature_map.fit(X, Y)
 
     def fit_koop(self,X,Y):
         assert hasattr(self, 'koop'), 'No koopman operator has been found'
-        Xp, Yp = self.phi_(X), self.phi_(Y)
+        Xp, Yp = self.feature_map(X), self.feature_map(Y)
         self.koop_.fit(Xp,Yp)
         self.is_koop_fitted = True
 
     def forecast(self, t, X, f=lambda x:x):
-        Xp = self.phi_(X)
+        Xp = self.feature_map(X)
         Yp = self.koop_.forecast(t, Xp, f=f)
         Y = self.decoder_(Yp)
         return Y
