d0d2e0b5067a2e9a101b80479a693f23e821a7b3
GregoirePacreau <32517738+GregoirePacreau@users.noreply.github.com>
2023-06-15 18:27:28 +0200

    updated test

--- kooplearn/feature_maps/Decoder.py
+++ kooplearn/feature_maps/Decoder.py
@@ -1,4 +1,9 @@
 class Decoder():
+    """
+    Decoder class, inverse operation of the feature map.
+
+    TODO: implement this class in the case where feature map is a DPNet.
+    """
     def fit_feature_map(self, X,Y):
         # may not need X or Y for setting up, here as placeholder in the general case
         pass
