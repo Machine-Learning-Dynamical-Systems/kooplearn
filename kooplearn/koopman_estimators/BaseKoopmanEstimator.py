cdcd15a13b74247716f4ca44345b7f52deea9829
Bruno <bruno.belucci-teixeira@dauphine.eu>
2023-06-15 14:36:41 +0200

    Create directories for feature maps and koopman estimators

---
+++ kooplearn/koopman_estimators/BaseKoopmanEstimator.py
@@ -0,0 +1,12 @@
+from sklearn.base import BaseEstimator
+
+
+class BaseKoopmanEstimator(BaseEstimator):
+    def __init__(self):
+        pass
+
+    def fit(self):
+        pass
+
+    def forecast(self):
+        pass
