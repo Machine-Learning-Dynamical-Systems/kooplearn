015796fcce939d2c9676d24836899686c747b5ae
Pietro Novelli <pietro.novelli@iit.it>
2023-07-08 20:27:50 +0200

    pde solving

--- .dev_exps/pde_integration/tri_well/tri_well.py
+++ .dev_exps/pde_integration/tri_well/tri_well.py
@@ -32,8 +32,12 @@ def generate_dataset(cfg: BaseConfigs, rng_seed: int = 42):
         dt = cfg.dt,
         rng_seed = rng_seed
     )
-    traj = ds.generate(np.array([-0.5]), cfg.num_samples, show_progress=True)
-    return traj[::cfg.take_every], ds._ref_eigenvalues, ds._ref_boltzmann_density, ds._ref_domain_sample
+    num_init_pts = 200
+    traj = ds.generate(np.linspace(-1, 1, num_init_pts), int(cfg.num_samples/num_init_pts), show_progress=True)
+    traj = traj[::cfg.take_every]
+    traj_in = traj[:-1].reshape(-1, 1)
+    traj_out = traj[1:].reshape(-1, 1)
+    return traj_in, traj_out, ds._ref_eigenvalues, ds._ref_boltzmann_density, ds._ref_domain_sample
 
 def objective(trial: optuna.Trial, training_traj: ArrayLike, ref_eigenvalues: ArrayLike):
     length_scale = trial.suggest_float("length_scale", 0.001, 0.2)
@@ -53,10 +57,11 @@ def objective(trial: optuna.Trial, training_traj: ArrayLike, ref_eigenvalues: Ar
     return np.linalg.norm(eigenvalues - np.sort(ref_eigenvalues))
 
 def main(cfg: BaseConfigs):
-    training_traj, ref_eigenvalues, ref_density, domain_sample = generate_dataset(cfg)
-    objective_fn = partial(objective, training_traj=training_traj, ref_eigenvalues=ref_eigenvalues)
-    study_name = "tri_well"
-    storage_name = "sqlite:///{}.db".format(study_name)
+    traj_in, traj_out, ref_eigenvalues, ref_density, domain_sample = generate_dataset(cfg)
+    print("Number of samples: {}".format(len(traj_in)))
+    # objective_fn = partial(objective, training_traj=training_traj, ref_eigenvalues=ref_eigenvalues)
+    # study_name = "tri_well"
+    # storage_name = "sqlite:///{}.db".format(study_name)
     #Optuna study
     # study = optuna.create_study(study_name=study_name, storage=storage_name, direction="minimize", load_if_exists=True)
     # study.optimize(objective_fn, n_trials=cfg.num_trials)
@@ -83,7 +88,8 @@ def main(cfg: BaseConfigs):
         tikhonov_reg = tikhonov_reg,
         solver = 'arnoldi'
     )
-    model = model.fit(training_traj[:-1], training_traj[1:])
+    
+    model = model.fit(traj_in, traj_out)
     eigenvalues = np.sort(model.eig())
     print(eigenvalues)
     print(np.sort(ref_eigenvalues))
@@ -94,8 +100,8 @@ def main(cfg: BaseConfigs):
         ref_density,
         domain_sample,
         scale = (eigenvalues[-1].real)**-1,
-        duration_in_seconds= 3.0,
-        fps = 12
+        duration_in_seconds= 10.0,
+        fps = 1
     )
 
 if __name__ == "__main__":
