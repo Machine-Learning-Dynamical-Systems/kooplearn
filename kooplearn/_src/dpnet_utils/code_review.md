a7486c4e3dbbf9ddb6ce6ff615bb9c7020a26cc0
Pietro Novelli <pietronvll@gmail.com>
2023-07-18 22:11:14 +0200

    Working on DPNets

--- kooplearn/_src/dpnet_utils/code_review.md
+++ kooplearn/_src/dpnet_utils/code_review.md
@@ -4,4 +4,3 @@ The code is far too complex to be putted in this form. Some comments:
 2. The `DPNetFeatureMap` should be a subclass of both `lightning.LightningModule` and `kooplearn._src.encoding_decoding_utils.TrainableFeatureMap`. They are compatible.
 3. The functionality currently contained in `DPNetModule` should be moved inside `DPNetFeatureMap`, which is currently only handling initialization.
 4. I provide a minimal example what I explained above, feel free to add the additional kwargs needed (although I do not really like the long list of arguments which is currently needed to initialize `DPNetFeatureMap`, + there are some arguments with the suffix `_2`, which are unintelligible to me)
-5. I have moved the `dpnet_loss` module inside `kooplearn._src.metrics
---
+++ kooplearn/_src/nn/torch_lightning/DPNet.py
@@ -0,0 +1,91 @@
+from kooplearn._src.models.abc import TrainableFeatureMap
+try:
+    import lightning
+except ImportError:
+    raise ImportError(f"Unable to import pytorch-lightning")
+
+class DPNetFeatureMap(TrainableFeatureMap):
+    def __init__(self, trainer: lightning.Trainer, *args, **kw):
+        self.trainer = trainer
+        self.lightning_module = _DPNetLightningModule(*args, **kw)
+    
+    def fit(self, X, Y):
+        self.trainer.fit(self.lightning_module, )
+
+class _DPNetLightningModule(lightning.LightningModule):
+    def __init__(self, model_class, model_hyperparameters,
+                 optimizer_fn, optimizer_hyperparameters, loss_fn,
+                 scheduler_fn=None, scheduler_hyperparameters=None, scheduler_config=None,
+                 model_class_2=None, model_hyperparameters_2=None,
+                 ):
+        super().__init__()
+        for k, v in model_hyperparameters.items():
+            self.hparams[k] = v
+        for k, v in model_hyperparameters_2.items():
+            self.hparams[f'model_2_{k}'] = v
+        for k, v in optimizer_hyperparameters.items():
+            self.hparams[f'optim_{k}'] = v
+        for k, v in scheduler_hyperparameters.items():
+            self.hparams[f'sched_{k}'] = v
+        for k, v in scheduler_config.items():
+            self.hparams[f'sched_{k}'] = v
+        self.save_hyperparameters()
+        self.model = model_class(**model_hyperparameters)
+        # If model_class_2 is None, the model_2 is the same as model (with shared weights)
+        if model_class_2 is not None:
+            self.model_2 = model_class_2(**model_hyperparameters_2)
+        else:
+            self.model_2 = self.model
+        self.optimizer_fn = optimizer_fn
+        self.scheduler_fn = scheduler_fn
+        self.optimizer_hyperparameters = optimizer_hyperparameters
+        self.scheduler_hyperparameters = scheduler_hyperparameters
+        self.scheduler_config = scheduler_config
+        self.loss_fn = loss_fn
+
+    def configure_optimizers(self):
+        optimizer = self.optimizer_fn(self.parameters(), **self.optimizer_hyperparameters)
+        if self.scheduler_fn is not None:
+            scheduler = self.scheduler_fn(optimizer, **self.scheduler_hyperparameters)
+            lr_scheduler_config = self.scheduler_config.copy()
+            lr_scheduler_config['scheduler'] = scheduler
+            return [optimizer], [lr_scheduler_config]
+        return optimizer
+
+    def training_step(self, train_batch, batch_idx):
+        outputs = self.base_step(train_batch, batch_idx)
+        return outputs
+
+    def validation_step(self, valid_batch, batch_idx):
+        outputs = self.base_step(valid_batch, batch_idx)
+        return outputs
+
+    def forward(self, batch):
+        # dimensions convention (..., channels, temporal_dim)
+        return self.model(batch)
+
+    def base_step(self, batch, batch_idx):
+        # dimensions convention (..., channels, temporal_dim)
+        # if any y is out of series, we do not use the sample from the batch
+        mask_out_of_series_left = batch['mask_out_of_series_left']
+        mask_out_of_series_right = batch['mask_out_of_series_right']
+        mask_out_of_series = mask_out_of_series_left + mask_out_of_series_right
+        x_value = batch['x_value']
+        y_value = batch['y_value']
+        batch_out_of_series = mask_out_of_series.any(dim=-1).any(dim=-1)
+        batch_in_series = ~batch_out_of_series
+
+        x_value = x_value[batch_in_series]
+        y_value = y_value[batch_in_series]
+        data_x = {'x_value': x_value}
+        data_y = {'x_value': y_value}
+        model_output_x = self.model(data_x)
+        x_encoded = model_output_x['x_encoded']
+        # Note that model_2 is the same as model in case we have not defined a second model
+        model_output_y = self.model_2(data_y)
+        y_encoded = model_output_y['x_encoded']
+        loss = self.loss_fn(x_encoded, y_encoded)
+        outputs = {
+            'loss': loss,
+        }
+        return outputs
