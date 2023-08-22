6ec0177d156795c527f46871dce887908440a5dd
Pietro Novelli <pietronvll@gmail.com>
2023-08-22 16:33:11 +0200

    Pushed read the docs config file

--- docs/conf.py
+++ docs/conf.py
@@ -7,13 +7,19 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'kooplearn'
-copyright = '2023, Pietro Novelli, Grégoire Pacreau, Bruno Belucci'
-author = 'Pietro Novelli, Grégoire Pacreau, Bruno Belucci'
+copyright = '2023, Pietro Novelli, Grégoire Pacreau, Bruno Belucci, Vladimir Kostic'
+author = 'Pietro Novelli, Grégoire Pacreau, Bruno Belucci, Vladimir Kostic'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.mathjax',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.viewcode',
+    'sphinx_design',
+]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
@@ -23,5 +29,5 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_book_theme'
 html_static_path = ['_static']
