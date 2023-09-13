68c80a2d6576238ef4796d6941a25b1bce2a4ed2
Pietro Novelli <pietronvll@gmail.com>
2023-09-13 17:18:46 +0200

    MNIST example data pipeline

---
+++ kooplearn/_src/check_deps.py
@@ -0,0 +1,17 @@
+def check_torch_deps():
+    try:
+        import lightning
+    except ImportError:
+        raise ImportError(
+            "To use kooplearn's deep learning models please reinstall it with the `torch` extra flag by typing `pip install kooplearn[torch]`."
+        )
+
+def check_dashboard_deps():
+    try:
+        import plotly
+        import dash_iconify
+        import dash_mantine_components
+    except ImportError:
+        raise ImportError(
+            "To use kooplearn's dashboard please reinstall it with the `dashboard` extra flag by typing `pip install kooplearn[dashboard]`."
+        )
--- kooplearn/models/feature_maps/__init__.py
+++ kooplearn/models/feature_maps/__init__.py
@@ -1,6 +1,5 @@
 from kooplearn.models.feature_maps.base import IdentityFeatureMap, ConcatenateFeatureMaps
 try:
-    from kooplearn.models.feature_maps.DPNets import DPNetsFeatureMap
+    from kooplearn.models.feature_maps.DPNets import DPNet
 except ImportError:
     pass
-#
