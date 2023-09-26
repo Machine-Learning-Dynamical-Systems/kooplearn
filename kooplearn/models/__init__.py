c4cba9e8f4367b5e246d9e55665fda0ae4b8b09c
Pietro Novelli <pietro.novelli@iit.it>
2023-09-26 22:08:52 +0200

    Updated Main fig

--- kooplearn/models/__init__.py
+++ kooplearn/models/__init__.py
@@ -1,5 +1,13 @@
-from kooplearn.models.edmd import DMD, ExtendedDMD  # isort:skip
-from kooplearn.models.ae.consistent import ConsistentAE
-from kooplearn.models.ae.dynamic import DynamicAE
-from kooplearn.models.deepedmd import DeepEDMD
+from kooplearn.models.edmd import DMD, ExtendedDMD
 from kooplearn.models.kernel import KernelDMD
+
+from kooplearn.models.deepedmd import DeepEDMD  # isort:skip
+
+try:
+    from kooplearn._src.check_deps import check_torch_deps
+
+    check_torch_deps()
+    from kooplearn.models.ae.consistent import ConsistentAE
+    from kooplearn.models.ae.dynamic import DynamicAE
+except:
+    pass
