511a13d479268fe383e3938a62a6f039828ff232
GregoirePacreau <gregoirepacreau@hotmail.fr>
2024-01-19 09:07:27 +0100

    saving small changes in util

--- kooplearn/quantile_regression/utils.py
+++ kooplearn/quantile_regression/utils.py
@@ -2,17 +2,16 @@ import numpy as np
 from sklearn.isotonic import IsotonicRegression
 from typing import Optional, Callable, Union
 
-
 def compute_quantile_robust(values:np.ndarray, cdf:np.ndarray, alpha:Union[str, float]='all', isotonic:bool=True, rescaling:bool=True):
-
+    # TODO: correct this code
     # correction of the cdf using isotonic regression
     if isotonic:
         for i in range(cdf.shape[0]):
-            cdf[i] = IsotonicRegression(y_min=0, y_max=cdf[i].max()).fit_transform(range(cdf.shape[1]), cdf[i])
+            cdf[i] = IsotonicRegression(y_min=0., y_max=cdf[i].max()).fit_transform(range(cdf.shape[1]), cdf[i])
     if rescaling:
         max_cdf = np.outer(cdf.max(axis=-1), np.ones(cdf.shape[1]))
-        min_cdf = np.outer(cdf.min(axis=-1), np.ones(cdf.shape[1]))
-        cdf = (cdf - min_cdf)/(max_cdf - min_cdf)
+        max_cdf[max_cdf == 0] = 1.    # security to avoid errors
+        cdf = cdf/max_cdf
 
     # if alpha = all, return the entire cdf
     if alpha=='all':
