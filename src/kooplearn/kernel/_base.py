Added singular values computation for Kernel and Nystroem classes + fixed some bugs

src/kooplearn/kernel/_base.py
@@ -187,7 +187,8 @@ class Kernel(BaseEstimator):
         "alpha": [
             [Interval(Real, 0, None, closed="left")],
             None,
-            ],        "eigen_solver": [StrOptions({"auto", "dense", "arpack", "randomized"})],
+            ],        
+        "eigen_solver": [StrOptions({"auto", "dense", "arpack", "randomized"})],
         "tol": [Interval(Real, 0, None, closed="left")],
         "max_iter": [
             Interval(Integral, 1, None, closed="left"),
@@ -341,7 +342,6 @@ class Kernel(BaseEstimator):
 
         self._fit_result = fit_result
         self.U_, self.V_, self._spectral_biases = fit_result.values()
-        self.X_fit_ = X
 
         logger.info(f"Fitted {self.__class__.__name__} model.")
         return self
@@ -372,9 +372,6 @@ class Kernel(BaseEstimator):
         X = validate_data(self, X, reset=False, copy=self.copy_X)
         X_fit, _ = self._split_trajectory(self.X_fit_)
         K_Xin_X = self._get_kernel(X, X_fit)
-        state_pred = _regressors.predict(
-            n_steps, self._fit_result, self.kernel_YX_, K_Xin_X, X_fit
-        )
 
         if observable is not None:
             observable = validate_data(self, observable, reset=False, copy=self.copy_X)
@@ -385,14 +382,16 @@ class Kernel(BaseEstimator):
                     f"{observable.shape[0]}"
                 )
             observable_fit, _ = self._split_trajectory(observable)
-            return _regressors.predict(
-                n_steps,
-                self._fit_result,
-                self.kernel_YX_,
-                K_Xin_X,
-                observable_fit,
-            )
-        return state_pred
+        else:
+            observable_fit = X_fit
+        pred = _regressors.predict(
+            n_steps,
+            self._fit_result,
+            self.kernel_YX_,
+            K_Xin_X,
+            observable_fit,
+        )
+        return pred
 
     def risk(self, X=None):
         """Compute the estimator risk.
@@ -418,16 +417,16 @@ class Kernel(BaseEstimator):
                 )
             X_val, Y_val = self._split_trajectory(X)
             X_train, Y_train = self._split_trajectory(self.X_fit_)
-            kernel_Yv = self.kernel(Y_val)
-            kernel_XXv = self.kernel(X_train, X_val)
-            kernel_YYv = self.kernel(Y_train, Y_val)
+            kernel_Yv = self._get_kernel(Y_val)
+            kernel_XXv = self._get_kernel(X_train, X_val)
+            kernel_YYv = self._get_kernel(Y_train, Y_val)
         else:
             kernel_Yv = self.kernel_Y_
             kernel_XXv = self.kernel_X_
             kernel_YYv = self.kernel_Y_
 
         return _regressors.estimator_risk(
-            kernel_Yv, self.kernel_Y_, kernel_XXv, kernel_YYv, self.U_, self.V_
+            self._fit_result, kernel_Yv, self.kernel_Y_, kernel_XXv, kernel_YYv,
         )
 
     def eig(self, eval_left_on=None, eval_right_on=None):
@@ -515,10 +514,11 @@ class Kernel(BaseEstimator):
             self._fit_result, self.kernel_X_, self.kernel_YX_
         )
         X_fit, _ = self._split_trajectory(self.X_fit_)
-        K_Xin_X = self.kernel(X, X_fit)
+        K_Xin_X = self._get_kernel(X, X_fit)
         _gamma = _regressors.estimator_modes(eig_result, K_Xin_X)
 
         if observable is not None:
+            observable = validate_data(self, observable, reset=False, copy=self.copy_X)
             if observable.shape[0] != self.X_fit_.shape[0]:
                 raise ValueError(
                     "'observable' should have the same number of samples "
@@ -526,8 +526,18 @@ class Kernel(BaseEstimator):
                     f"{observable.shape[0]}"
                 )
             observable_fit, _ = self._split_trajectory(observable)
-            return np.tensordot(_gamma, observable_fit, axes=1), eig_result
-        return np.tensordot(_gamma, X_fit, axes=1), eig_result
+        else:
+            observable_fit = X_fit
+        return np.tensordot(_gamma, observable_fit, axes=1), eig_result
+
+    def svals(self):
+        """Singular values of the Koopman/Transfer operator.
+
+        Returns:
+            The estimated singular values of the Koopman/Transfer operator. Array of shape `(n_components,)`.
+        """
+        check_is_fitted(self)
+        return _regressors.svdvals(self._fit_result, self.kernel_X_, self.kernel_Y_)
 
     def _get_kernel(self, X, Y=None):
         """Compute the pairwise kernel matrix."""
@@ -539,6 +549,8 @@ class Kernel(BaseEstimator):
                 "degree": self.degree,
                 "coef0": self.coef0,
             }
+        if Y is None:
+            Y = X
         return pairwise_kernels(
             X,
             Y,
@@ -565,6 +577,7 @@ class Kernel(BaseEstimator):
                 f"{1 + self.lag_time_} are required."
             )
         self.gamma_ = 1 / X.shape[1] if self.gamma is None else self.gamma
+        self.X_fit_ = X
         X_fit, Y_fit = self._split_trajectory(X)
         self.kernel_X_, self.kernel_Y_, self.kernel_YX_ = self._init_kernels(
             X_fit, Y_fit

src/kooplearn/kernel/_nystroem.py
@@ -297,7 +297,6 @@ class NystroemKernel(BaseEstimator):
 
         self._fit_result = fit_result
         self.U_, self.V_, self._spectral_biases = fit_result.values()
-        self.X_fit_ = X
 
         logger.info(f"Fitted {self.__class__.__name__} model.")
         return self
@@ -329,9 +328,6 @@ class NystroemKernel(BaseEstimator):
         X_fit, _ = self._split_trajectory(self.X_fit_)
         X_fit = X_fit[self.nys_centers_idxs_]
         K_Xin_X = self._get_kernel(X, X_fit)
-        state_pred = _regressors.predict(
-            n_steps, self._fit_result, self.kernel_YX_, K_Xin_X, X_fit
-        )
 
         if observable is not None:
             observable = validate_data(self, observable, reset=False, copy=self.copy_X)
@@ -342,14 +338,17 @@ class NystroemKernel(BaseEstimator):
                     f"{observable.shape[0]}"
                 )
             observable_fit, _ = self._split_trajectory(observable)
-            return _regressors.predict(
-                n_steps,
-                self._fit_result,
-                self.kernel_YX_,
-                K_Xin_X,
-                observable_fit[self.nys_centers_idxs_],
-            )
-        return state_pred
+            observable_fit = observable_fit[self.nys_centers_idxs_]
+        else:
+            observable_fit = X_fit
+        pred = _regressors.predict(
+            n_steps,
+            self._fit_result,
+            self.kernel_YX_,
+            K_Xin_X,
+            observable_fit,
+        )
+        return pred
 
     def risk(self, X=None):
         """Compute the estimator risk.
@@ -485,6 +484,15 @@ class NystroemKernel(BaseEstimator):
             )
         return (np.tensordot(_gamma, X_fit, axes=1), eig_result)
 
+    def svals(self):
+        """Singular values of the Koopman/Transfer operator.
+
+        Returns:
+            The estimated singular values of the Koopman/Transfer operator. Array of shape `(n_components,)`.
+        """
+        check_is_fitted(self)
+        return _regressors.svdvals(self._fit_result, self.kernel_X_, self.kernel_Y_)
+
     def _get_kernel(self, X, Y=None):
         """Compute the pairwise kernel matrix."""
         if callable(self.kernel):
@@ -495,6 +503,8 @@ class NystroemKernel(BaseEstimator):
                 "degree": self.degree,
                 "coef0": self.coef0,
             }
+        if Y is None:
+            Y = X
         return pairwise_kernels(
             X,
             Y,
@@ -521,6 +531,7 @@ class NystroemKernel(BaseEstimator):
                 f"{1 + self.lag_time_} are required."
             )
         self.gamma_ = 1 / X.shape[1] if self.gamma is None else self.gamma
+        self.X_fit_ = X
         X_fit, Y_fit = self._split_trajectory(X)
 
         # Perform random center selection

src/kooplearn/kernel/_regressors.py
@@ -229,6 +229,20 @@ def predict(
     return np.linalg.multi_dot([K_dot_U, M, V_dot_obs])
 
 
+def svdvals(fit_result, K_X, K_Y):
+    U = fit_result["U"]
+    V = fit_result["V"]
+    # Inefficient implementation
+    rdim = (K_X.shape[0]) ** (-1)
+    A = np.linalg.multi_dot([V.T, rdim * K_Y, V])
+    B = np.linalg.multi_dot([U.T, rdim * K_X, U])
+    v = eig(A @ B, left=False, right=False)
+    # Clip the negative values
+    v = v.real
+    v[v < 0] = 0
+    return np.sqrt(v)
+
+
 def pcr(
     kernel_X: ndarray,
     tikhonov_reg: float = 0.0,
@@ -580,7 +594,7 @@ def rand_reduced_rank(
     rank: int,
     n_oversamples: int = 5,
     optimal_sketching: bool = False,
-    iterated_power: Literal["auto"] | int = "auto",
+    iterated_power: int = 1,
     rng_seed: int | None = None,
     precomputed_cholesky=None,
 ) -> FitResult: