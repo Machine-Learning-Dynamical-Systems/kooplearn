2fcb209eace53b1f90acd836c11b0be1e8363a76
Pietro Novelli <pietro.novelli@iit.it>
2025-10-27 18:35:04 +0100

    Working on Ordered MNIST

--- src/kooplearn/torch/nn/_base.py
+++ src/kooplearn/torch/nn/_base.py
@@ -14,32 +14,8 @@ __all__ = [
     "VampLoss",
 ]
 
-# Losses_____________________________________________________________________________________________
 
-
-class _RegularizedLoss(Module):
-    """Base class for regularized losses.
-
-    Args:
-        gamma (float, optional): Regularization strength.
-        regularizer (literal, optional): Regularizer. Either :func:`orthn_fro <linear_operator_learning.nn.functional.orthonormal_fro_reg>` or :func:`orthn_logfro <linear_operator_learning.nn.functional.orthonormal_logfro_reg>`. Defaults to :func:`orthn_fro <linear_operator_learning.nn.functional.orthonormal_fro_reg>`.
-    """
-
-    def __init__(
-        self, gamma: float, regularizer: Literal["orthn_fro", "orthn_logfro"]
-    ) -> None:  # TODO: Automatically determine 'gamma' from dim_x and dim_y
-        super().__init__()
-        self.gamma = gamma
-
-        if regularizer == "orthn_fro":
-            self.regularizer = F.orthonormal_fro_reg
-        elif regularizer == "orthn_logfro":
-            self.regularizer = F.orthonormal_logfro_reg
-        else:
-            raise NotImplementedError(f"Regularizer {regularizer} not supported!")
-
-
-class VampLoss(_RegularizedLoss):
+class VampLoss(Module):
     r"""Variational Approach for learning Markov Processes (VAMP) score by :cite:t:`Wu2019`.
 
     .. math::
@@ -49,18 +25,14 @@ class VampLoss(_RegularizedLoss):
     Args:
         schatten_norm (int, optional): Computes the VAMP-p score with ``p = schatten_norm``. Defaults to 2.
         center_covariances (bool, optional): Use centered covariances to compute the VAMP score. Defaults to True.
-        gamma (float, optional): Regularization strength. Defaults to 1e-3.
-        regularizer (literal, optional): Regularizer. Either :func:`orthn_fro <linear_operator_learning.nn.functional.orthonormal_fro_reg>` or :func:`orthn_logfro <linear_operator_learning.nn.functional.orthonormal_logfro_reg>`. Defaults to :func:`orthn_fro <linear_operator_learning.nn.functional.orthonormal_fro_reg>`.
     """
 
     def __init__(
         self,
         schatten_norm: int = 2,
         center_covariances: bool = True,
-        gamma: float = 1e-3,
-        regularizer: Literal["orthn_fro", "orthn_logfro"] = "orthn_fro",
     ) -> None:
-        super().__init__(gamma, regularizer)
+        super().__init__()
         self.schatten_norm = schatten_norm
         self.center_covariances = center_covariances
 
@@ -84,10 +56,10 @@ class VampLoss(_RegularizedLoss):
             y,
             self.schatten_norm,
             self.center_covariances,
-        ) + self.gamma * (self.regularizer(x) + self.regularizer(y))
+        )
 
 
-class SpectralContrastiveLoss(_RegularizedLoss):
+class SpectralContrastiveLoss(Module):
     r"""Spectral contrastive loss based originally introduced by :cite:t:`haochen2021provable`, and adopted for evolution operators in :cite:t:`turri2025self, jeong2025efficient`
 
     .. math::
@@ -95,16 +67,14 @@ class SpectralContrastiveLoss(_RegularizedLoss):
         \mathcal{L}(x, y) = \frac{1}{N(N-1)}\sum_{i \neq j}\langle x_{i}, y_{j} \rangle^2 - \frac{2}{N}\sum_{i=1}\langle x_{i}, y_{i} \rangle.
 
     Args:
-        gamma (float, optional): Regularization strength. Defaults to 1e-3.
+        gamma (float, optional): Regularization strength. Defaults to 0.0 (no regularization).
         regularizer (literal, optional): Regularizer. Either :func:`orthn_fro <linear_operator_learning.nn.functional.orthonormal_fro_reg>` or :func:`orthn_logfro <linear_operator_learning.nn.functional.orthonormal_logfro_reg>`. Defaults to :func:`orthn_fro <linear_operator_learning.nn.functional.orthonormal_fro_reg>`.
     """
 
     def __init__(
         self,
-        gamma: float = 1e-3,
-        regularizer: Literal["orthn_fro", "orthn_logfro"] = "orthn_fro",
     ) -> None:
-        super().__init__(gamma, regularizer)
+        super().__init__()
 
     def forward(self, x: Tensor, y: Tensor) -> Tensor:
         """Forward pass of the L2 contrastive loss.
@@ -119,12 +89,10 @@ class SpectralContrastiveLoss(_RegularizedLoss):
 
             ``y``: :math:`(N, D)`, where :math:`N` is the batch size and :math:`D` is the number of features.
         """
-        return F.l2_contrastive_loss(x, y) + self.gamma * (
-            self.regularizer(x) + self.regularizer(y)
-        )
+        return F.spectral_contrastive_loss(x, y)
 
 
-class KLContrastiveLoss(_RegularizedLoss):
+class KLContrastiveLoss(Module):
     r"""NCP/Contrastive/Mutual Information Loss based on the KL divergence.
 
     .. math::
@@ -132,7 +100,7 @@ class KLContrastiveLoss(_RegularizedLoss):
         \mathcal{L}(x, y) = \frac{1}{N(N-1)}\sum_{i \neq j}\langle x_{i}, y_{j} \rangle - \frac{2}{N}\sum_{i=1}\log\big(\langle x_{i}, y_{i} \rangle\big).
 
     Args:
-        gamma (float, optional): Regularization strength. Defaults to 1e-3.
+        gamma (float, optional): Regularization strength. Defaults to 0.0 (no regularization).
         regularizer (literal, optional): Regularizer. Either :func:`orthn_fro <linear_operator_learning.nn.functional.orthonormal_fro_reg>` or :func:`orthn_logfro <linear_operator_learning.nn.functional.orthonormal_logfro_reg>`. Defaults to :func:`orthn_fro <linear_operator_learning.nn.functional.orthonormal_fro_reg>`.
 
 
@@ -140,10 +108,8 @@ class KLContrastiveLoss(_RegularizedLoss):
 
     def __init__(
         self,
-        gamma: float = 1e-3,
-        regularizer: Literal["orthn_fro", "orthn_logfro"] = "orthn_fro",
     ) -> None:
-        super().__init__(gamma, regularizer)
+        super().__init__()
 
     def forward(self, x: Tensor, y: Tensor) -> Tensor:
         """Forward pass of the KL contrastive loss.
@@ -158,9 +124,7 @@ class KLContrastiveLoss(_RegularizedLoss):
 
             ``y``: :math:`(N, D)`, where :math:`N` is the batch size and :math:`D` is the number of features.
         """
-        return F.kl_contrastive_loss(x, y) + self.gamma * (
-            self.regularizer(x) + self.regularizer(y)
-        )
+        return F.kl_contrastive_loss(x, y)
 
 
 class AutoEncoderLoss(Module):
